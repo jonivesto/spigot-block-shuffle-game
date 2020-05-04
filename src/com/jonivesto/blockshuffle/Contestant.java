81066101ba1f261a643000f10cee3f10f99f81f7
jonivesto <n7vejo00@students.oamk.fi>
2020-05-04 15:38:32 +0300

    Add scoreboards

---
+++ src/com/jonivesto/blockshuffle/Contestant.java
@@ -0,0 +1,9 @@
+package com.jonivesto.blockshuffle;
+
+import org.bukkit.Material;
+
+public class Contestant {
+	int points = 0;	
+	Material currentTarget;
+	Material previousTarget;
+}
--- src/com/jonivesto/blockshuffle/Main.java
+++ src/com/jonivesto/blockshuffle/Main.java
@@ -1,54 +1,68 @@
 package com.jonivesto.blockshuffle;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
 
-public class Main extends JavaPlugin {
-	
-	BukkitScheduler scheduler;
-	
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin implements Listener {
+
+	// Defines if there is a running game in progress
 	boolean gameActive = false;
+	boolean blockFound = true;
+	
+	// This object shuffles the blocks:
+	Random random = new Random();
+	
+	// List of players in active game.
+	// The String key is the player's UUID
+	Map<String, Contestant> contestants = new HashMap<>();
 	
 	@Override
     public void onEnable() {
-		// Run checks every 10 ticks
-		scheduler = getServer().getScheduler();
-        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-                runChecks();
-            }
-        }, 0L, 10L);
-    }
-	
-    protected void runChecks() {
-		if(!gameActive) return;
-		
-		// TODO: checks
+		getServer().getPluginManager().registerEvents(this, this);   
 	}
 
 	@Override
-    public void onDisable() {
-		scheduler.cancelTasks(this);
-    }
+    public void onDisable() {}
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) { 	
     	// Stop command
         if (command.getName().equalsIgnoreCase("stopblockshuffle")) {
-            sender.sendMessage("§a§lYou stopped the game.");
+        	// Return if there is no game to abort
+        	if(!gameActive) return true;
+        	// Stop game
             stopGame();
+            // Inform all players who stopped the game:
+            for(Player player : getServer().getOnlinePlayers()) {
+            	player.sendMessage(sender.getName() + ChatColor.RED + " Aborted the game!");
+            }
             return true;
         }       
         // Play command
         else if (command.getName().equalsIgnoreCase("playblockshuffle")) {
-            sender.sendMessage("§a§lYou started the game.");
+        	// Return if game already active
+        	if(gameActive) return true;
             newGame();
             return true;
         }
@@ -57,29 +71,136 @@ public class Main extends JavaPlugin {
 
 	private void stopGame() {
 		gameActive = false;
-		
+		blockFound = true;
 	}
 
-	private void newGame() {	
-		shuffle();	
-		gameActive = true;
-	}
-
-	private void shuffle() {
-		// This object shuffles the blocks:
-		Random random = new Random();
-		// Assign new block for each player:
+	private void newGame() {
+		contestants.clear();
+		// Only players who are online when start command runs are added to the game
 		for(Player player : getServer().getOnlinePlayers()) {
-			// Get the limit for the range used for shuffle:
-			int length = Material.values().length-1;
-			// Shuffle until the result is a block:
-			Material material = Material.values()[random.nextInt(length)];
-			while(material.isBlock()==false) {
-				material = Material.values()[random.nextInt(length)];
+			contestants.put(player.getUniqueId().toString(), new Contestant());
+			// Set survival gamemode if not current:
+			if(player.getGameMode()!=GameMode.SURVIVAL) {
+				player.setGameMode(GameMode.SURVIVAL);
 			}
-			// Tell player their new block:
-			player.sendMessage("§a§lYour new block is §6§l" + material.toString());
+			// Inform player that game has started:
+			player.sendMessage(ChatColor.DARK_PURPLE +"You were added to a new game of block shuffle!");
+		}
+		// Set game active so no one can join it anymore
+		gameActive = true;
+		// Start game flow by giving the blocks
+		shuffle();		
+	}
+	
+	// Randomly picks blocks for the shuffle() method:
+    private Material getRandomMaterial() {
+        // Get the limit for the range used for shuffle:
+        int length = Material.values().length-1;
+        // Shuffle until the result is a block:
+        Material material = Material.values()[random.nextInt(length)];
+        while(material.name().contains("END_")
+            ||material.name().contains("COMMAND")
+            ||material.name().contains("BARRIER")
+            ||material.name().contains("PURPUR")
+            ||material.name().contains("DRAGON")
+            ||material.name().contains("STRUCTURE")
+            ||material.name().contains("CHORUS")
+            ||material.name().contains("BEACON")
+            ||material.name().contains("HEAD")
+            ||material.name().contains("SHULKER")
+            ||material.name().contains("ENDER_")
+            ||material.isBlock()==false
+            ||material.isSolid()==false) {
+            material = Material.values()[random.nextInt(length)];
+        }
+        return material;
+	}
+	
+	private void shuffle() {	
+		// Assign new block for each player in contestants list
+		for(Contestant contestant : contestants.values()) {
+			
+			contestant.previousTarget = contestant.currentTarget;
+			contestant.currentTarget = getRandomMaterial();
+		}	
+		// Inform the players about their new blocks.
+		// Only sent to players that are online.
+		for(Player player : getServer().getOnlinePlayers()) {
+			if(contestants.containsKey(player.getUniqueId().toString())) {
+				Material material = contestants.get(player.getUniqueId().toString()).currentTarget;
+				player.sendMessage("§a§lYour new block is §6§l" + material.name());
+			}	
 		}
 		
+		updateScoreboard();
+		blockFound = false;
+	}
+	
+	@EventHandler
+    public void join(PlayerJoinEvent e){
+		// Set spectator mode if game is active and player is not in contestants list
+		if(gameActive) {
+			Player player = e.getPlayer();
+			if(!contestants.containsKey(player.getUniqueId().toString())) {
+				player.setGameMode(GameMode.SPECTATOR);
+			}
+		}
+		else {
+			Player player = e.getPlayer();
+			player.setGameMode(GameMode.SURVIVAL);
+		}	
+    }
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		// Return if block is found
+		if(blockFound) return;
+		// Return if player did not move
+	    if (e.getTo().getBlockX() == e.getFrom().getBlockX() && e.getTo().getBlockY() == e.getFrom().getBlockY() && e.getTo().getBlockZ() == e.getFrom().getBlockZ()) return;   
+	    // See who the player is
+	    Player player = (Player) e.getPlayer();
+	    Contestant contestant = contestants.get(player.getUniqueId().toString());    
+	    // Return if player is not contestant
+	    if(contestants.containsKey(player.getUniqueId().toString())==false) {
+	    	return;
+	    }
+	    // Return if  there is no target block    
+	    if(contestant.currentTarget==null) {
+	    	return;
+	    }
+	    // Check if block player is standing on is their target block
+	    if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == contestant.currentTarget) {
+	    	blockFound(player.getName(), contestant);   	
+	    }
+	}
+
+	private void blockFound(String contestantName, Contestant contestant) {
+		blockFound = true;
+		contestant.points++;
+		// Inform others:
+		for(Player player : getServer().getOnlinePlayers()) {
+			player.sendMessage(contestantName + " found their block " + contestant.currentTarget.name() + "!");			
+		}
+		// Shuffle new blocks:
+		shuffle();
+	}
+	
+	private void updateScoreboard() {
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+	    Scoreboard scoreboard = manager.getNewScoreboard();	
+	    @SuppressWarnings("deprecation")
+		Objective objective = scoreboard.registerNewObjective("Blocks found", "");        
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        objective.setDisplayName(ChatColor.GOLD + "Blocks found");
+        
+        
+        for(Player player : getServer().getOnlinePlayers()) {
+			if(contestants.containsKey(player.getUniqueId().toString())) {
+				Score score = objective.getScore(player.getName());
+		        score.setScore(contestants.get(player.getUniqueId().toString()).points);
+			}
+			
+			player.setScoreboard(scoreboard);
+		}
 	}
 }
